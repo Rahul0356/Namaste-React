5cf2bb5c41453685e5ce6b0c36c8456627950950
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-20 12:10:00 +0530

    complete episode-03 (Lying the foundation)

---
+++ Episode-03/basic react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rough from './components/Rough'
+
+const App = () => {
+  return (
+   
+    <div>
+      <Rough/>
+    </div>
+  )
+}
+
+export default App
---
+++ Episode-03/basic react/src/components/Rough.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Rough = () => {
+  return (
+    // functional based components
+    <div>
+        <h1>Namaste React From functinal based components</h1>
+    </div>
+  )
+}
+
+export default Rough
