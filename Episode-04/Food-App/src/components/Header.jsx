29dfbda4ce6cbc4d3028b16ed0ab1465be36b04a
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-24 11:59:06 +0530

    add episode-06

--- Episode-04/Food-App/src/components/Header.jsx
+++ Episode-04/Food-App/src/components/Header.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const [btnNameReact, setBtnNameReact] = useState('Login');
+    console.log('header render');
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img src={LOGO_URL} alt="App Logo" className="logo" />
+
+      <div className="header">
+        <div className="logo-container">
+          <img src={LOGO_URL} alt="App Logo" className="logo" />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+            <button
+              className="loginBtn"
+              onClick={() => {
+                //   btnName = 'Logout';
+                btnNameReact === 'Login'
+                  ? setBtnNameReact('Logout')
+                  : setBtnNameReact('Login');
+                console.log(btnNameReact);
+              }}
+            >
+              {btnNameReact}
+            </button>
+          </ul>
+        </div>
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Header;
--- Episode-04/Food-App/src/components/Shimmer.jsx
+++ Episode-04/Food-App/src/components/Shimmer.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <div className='Shimmer-container'>
-        <div className='shimmer-cards'>cards</div>
-        <div className='shimmer-cards'>cards</div>
-        <div className='shimmer-cards'>cards</div>
-      
-
-
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
     </div>
   )
 }
